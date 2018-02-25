44dcc7feb9830849cfd93fe618000558ef0e826c
Shridhar Sukhani <sukha014@umn.edu>
2018-02-24 22:08:29 -0600

    Implemented win/lose logic

--- src/robot.cc
+++ src/robot.cc
@@ -60,8 +60,8 @@ void Robot::HandleCollision(EntityType object_type, ArenaEntity * object) {
     case kObstacle: lives_--;
                     break;
     case kBase: object->change_color();
-                if (static_cast<Base>(object)->IsCaptured() == false) {
-                  static_cast<Base>(object)->set_captured(true);
+                if (static_cast<Base *>(object)->IsCaptured() == false) {
+                  static_cast<Base *>(object)->set_captured(true);
                   num_bases_captured
                 }
                 break;
