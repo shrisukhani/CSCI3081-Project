91c6f3f95a8ce289ea218864977f0212a8014268
Shridhar Sukhani <shridharsukhani@MacBook-Pro-2.local>
2018-03-04 11:27:47 -0600

    Fingers Crossed. Please work.

--- src/obstacle.h
+++ src/obstacle.h
@@ -43,7 +43,7 @@ class Obstacle : public ArenaMobileEntity {
 
   void TimestepUpdate(unsigned int dt) override;
 
-  void HandleCollision(EntityType object_type, ArenaEntity * object = NULL) override;
+  void HandleCollision(EntityType object_type, ArenaEntity * object = nullptr) override;
 
   /**
    * @brief Get the name of the Obstacle for visualization purposes, and to
